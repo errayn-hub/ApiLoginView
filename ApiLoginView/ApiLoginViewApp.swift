02e27bacfd5529085fab5053028cdcf4b02f7140
errayn-hub <128374386+errayn-hub@users.noreply.github.com>
2023-05-09 10:16:22 +0300

    Initial Commit

---
+++ ApiLoginView/ApiLoginViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApiLoginViewApp.swift
+//  ApiLoginView
+//
+//  Created by ErRayn on 9.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ApiLoginViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ApiLoginView/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ApiLoginView
+//
+//  Created by ErRayn on 9.05.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
